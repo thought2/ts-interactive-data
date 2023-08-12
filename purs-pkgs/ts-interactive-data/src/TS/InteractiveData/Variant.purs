6b74fa0f0e697b454b07c37a9ae7e9aa11031c85
Michael Bock <no-day@posteo.net>
2023-08-12 08:56:27 +0200

    Finish variant

--- purs-pkgs/ts-interactive-data/src/TS/InteractiveData/Variant.purs
+++ purs-pkgs/ts-interactive-data/src/TS/InteractiveData/Variant.purs
@@ -1,28 +1,30 @@
-module TS.InteractiveData.Variant where
+module TS.InteractiveData.Variant
+  ( AnyA(..)
+  , AnyMsg(..)
+  , AnySta(..)
+  , Case(..)
+  , Key(..)
+  , Obj(..)
+  , variant_
+  ) where
 
 import Prelude
 
 import Chameleon.Impl.ReactBasic (ReactHtml)
-import Data.Array as Array
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Newtype (un)
-import Data.Traversable (class Foldable, class Traversable, for)
-import Data.Tuple (Tuple(..))
-import Data.Tuple.Nested (type (/\), (/\))
+import Data.Traversable (class Foldable, class Traversable)
 import Data.Variant (Variant)
 import Data.Variant as V
 import DataMVC.Types (DataUI(..), DataUiInterface(..), DataResult)
 import DataMVC.Types.DataUI (runDataUiFinal)
 import Foreign (Foreign)
 import Foreign.Object (Object)
-import Foreign.Object as Obj
 import Foreign.Object as Object
 import InteractiveData (DataUI, IDSurface)
 import InteractiveData.App.WrapData (WrapMsg, WrapState)
-import InteractiveData.DataUIs.Record as R
 import InteractiveData.DataUIs.Variant as Var
 import InteractiveData.Run.Types.HtmlT (IDHtmlT)
-import MVC.Record (ViewResult)
 import MVC.Variant (CaseKey(..))
 import Partial.Unsafe (unsafePartial)
 import Safe.Coerce (coerce)
@@ -64,34 +66,6 @@ keys :: forall v. Obj v -> Array Key
 keys (Obj obj) =
   coerce $ Object.keys obj
 
-modify :: forall v. (v -> v) -> Key -> Obj v -> Obj v
-modify f (Key key) (Obj obj) =
-  Obj $ Object.update (f >>> Just) key obj
-
-toIter :: forall v. Obj v -> Array (Key /\ v)
-toIter (Obj obj) =
-  coerce $ (Obj.toUnfoldable obj :: Array _)
-
-zip :: forall v1 v2 v3. (v1 -> v2 -> v3) -> Obj v1 -> Obj v2 -> Obj v3
-zip f (Obj obj1) (Obj obj2) =
-  let
-    obj1Iter :: Array (Key /\ v1)
-    obj1Iter = coerce (Obj.toUnfoldable obj1 :: Array _)
-
-    obj2Iter :: Array (Key /\ v2)
-    obj2Iter = coerce (Obj.toUnfoldable obj2 :: Array _)
-
-    obj3Iter :: Array (Key /\ v3)
-    obj3Iter = Array.zipWith
-      (\(key /\ v1) (_ /\ v2) -> key /\ f v1 v2)
-      obj1Iter
-      obj2Iter
-
-    obj3Iter' :: Array (String /\ v3)
-    obj3Iter' = coerce obj3Iter
-  in
-    Obj $ Obj.fromFoldable obj3Iter'
-
 variant_
   :: Key
   -> Obj (RDataUI AnyMsg AnySta AnyA)
@@ -114,7 +88,34 @@ variantItf_
 variantItf_ initKey dataUiItfs =
   let
     update :: VariantMsg Unit msg -> Case sta -> Case sta
-    update _ = unsafeCoerce 1
+    update = V.case_ # V.onMatch
+      { childCaseMsg: \(Case keyMsg valSta) (Case keySta valSta') ->
+          let
+            itf :: DataUiInterface Surface msg sta a
+            itf = lookup keyMsg dataUiItfs
+
+            update' :: msg -> sta -> sta
+            update' = itf # un DataUiInterface # _.update
+
+            val :: sta
+            val = update' valSta valSta'
+          in
+            Case keySta val
+
+      , changeCase: \(Case keyMsg _) _ ->
+          let
+            itf :: DataUiInterface Surface msg sta a
+            itf = lookup keyMsg dataUiItfs
+
+            init' :: Maybe a -> sta
+            init' = itf # un DataUiInterface # _.init
+
+            val :: sta
+            val = init' Nothing
+          in
+            Case keyMsg val
+      , errorMsg: unsafeCoerce 3
+      }
 
     extract :: Case sta -> DataResult (Case a)
     extract (Case key val) =
